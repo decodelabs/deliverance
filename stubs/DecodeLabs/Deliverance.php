e83a49a495aa9da9d17f6ad811adc71044743200
Tom Wright <tom@inflatablecookie.com>
2022-11-30 09:32:04 +0000

    Updated stub
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- stubs/DecodeLabs/Deliverance.php
+++ stubs/DecodeLabs/Deliverance.php
@@ -5,12 +5,13 @@
  */
 namespace DecodeLabs;
 
-use DecodeLabs\Veneer\Proxy;
-use DecodeLabs\Veneer\ProxyTrait;
+use DecodeLabs\Veneer\Proxy as Proxy;
+use DecodeLabs\Veneer\ProxyTrait as ProxyTrait;
 use DecodeLabs\Deliverance\Context as Inst;
 use DecodeLabs\Deliverance\Channel as Ref0;
-use DecodeLabs\Deliverance\Channel\Buffer as Ref1;
-use DecodeLabs\Deliverance\Broker as Ref2;
+use DecodeLabs\Deliverance\Channel\Stream as Ref1;
+use DecodeLabs\Deliverance\Channel\Buffer as Ref2;
+use DecodeLabs\Deliverance\Broker as Ref3;
 
 class Deliverance implements Proxy
 {
@@ -24,31 +25,31 @@ class Deliverance implements Proxy
     public static function openStream($stream, string $mode = 'a+'): Ref0 {
         return static::$instance->openStream(...func_get_args());
     }
-    public static function openCliInputStream(): Ref0 {
+    public static function openCliInputStream(): Ref1 {
         return static::$instance->openCliInputStream();
     }
-    public static function openCliOutputStream(): Ref0 {
+    public static function openCliOutputStream(): Ref1 {
         return static::$instance->openCliOutputStream();
     }
-    public static function openCliErrorStream(): Ref0 {
+    public static function openCliErrorStream(): Ref1 {
         return static::$instance->openCliErrorStream();
     }
-    public static function openHttpInputStream(): Ref0 {
+    public static function openHttpInputStream(): Ref1 {
         return static::$instance->openHttpInputStream();
     }
-    public static function openHttpOutputStream(): Ref0 {
+    public static function openHttpOutputStream(): Ref1 {
         return static::$instance->openHttpOutputStream();
     }
-    public static function newBuffer(?string $buffer = NULL): Ref1 {
+    public static function newBuffer(?string $buffer = NULL): Ref2 {
         return static::$instance->newBuffer(...func_get_args());
     }
-    public static function newBroker(): Ref2 {
+    public static function newBroker(): Ref3 {
         return static::$instance->newBroker();
     }
-    public static function newCliBroker(): Ref2 {
+    public static function newCliBroker(): Ref3 {
         return static::$instance->newCliBroker();
     }
-    public static function newHttpBroker(): Ref2 {
+    public static function newHttpBroker(): Ref3 {
         return static::$instance->newHttpBroker();
     }
 };
