a04b73acff6c5a11bfa3a65c176f4a08883164a4
Tom Wright <tom@inflatablecookie.com>
2025-11-04 12:30:49 +0000

    Improved doc comment headers
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Deliverance/DataReceiver.php
+++ src/Deliverance/DataReceiver.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package Deliverance
- * @license http://opensource.org/licenses/MIT
+ * Deliverance
+ * @license https://opensource.org/licenses/MIT
  */
 
 declare(strict_types=1);
